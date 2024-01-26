50c7cdd5ba0be8cca3e6a220dda604caef38ef9f
Salman-sajib <sfsajib@gmail.com>
2024-01-27 00:06:46 +0600

    added firebase to the project with real-time database

--- main.js
+++ main.js
@@ -1,11 +1,28 @@
 // ========== Import css file ==========
 import './style.css';
 
+// Add firebase
+
+import { initializeApp } from 'firebase/app';
+import { getDatabase, ref, push, onValue, remove } from 'firebase/database';
+
+const appSettings = {
+  databaseURL:
+    'https://shopping-list-6953c-default-rtdb.asia-southeast1.firebasedatabase.app/',
+};
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const moviesInDB = ref(database, 'movies');
+
+// ====================
+
 // Global variables
 const inputField = document.querySelector('#input-field');
 const addToListBtn = document.querySelector('#add-button');
 
 addToListBtn.addEventListener('click', (e) => {
-  console.log(e.target);
-  console.log(inputField.value);
+  let inputValue = inputField.value;
+  push(moviesInDB, inputValue);
+  console.log(`${inputValue} added to database`);
 });
